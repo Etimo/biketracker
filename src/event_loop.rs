3c037c2920696a9d5bdebfcac463a4b7ab554a67
Teo Klestrup Röijezon <teo@nullable.se>
2019-02-07 13:06:59 +0100

    Streamify the event loop

--- src/event_loop.rs
+++ src/event_loop.rs
@@ -1,38 +1,17 @@
 // Based on event loop example from https://github.com/PistonDevelopers/conrod/blob/master/examples/support/mod.rs
 
 use conrod::backend::glium::glium::glutin::{Event, EventsLoop};
-use std::{
-    thread::sleep, time::{Duration, Instant},
-};
+use futures::prelude::*;
+use std::time::Duration;
+use tokio::timer::{self, Interval};
 
-pub struct EventLoop {
-    last_update: Instant,
-}
-
-impl EventLoop {
-    pub fn new() -> Self {
-        EventLoop {
-            last_update: Instant::now(),
-        }
-    }
-
-    /// Produce an iterator yielding all available events.
-    pub fn next(&mut self, events_loop: &mut EventsLoop) -> Vec<Event> {
-        // We don't want to loop any faster than 60 FPS, so wait until it has been at least 16ms
-        // since the last yield.
-        let last_update = self.last_update;
-        let sixteen_ms = Duration::from_millis(16);
-        let duration_since_last_update = Instant::now().duration_since(last_update);
-        if duration_since_last_update < sixteen_ms {
-            sleep(sixteen_ms - duration_since_last_update);
-        }
-
-        // Collect all pending events.
+pub fn event_stream(
+    mut events_loop: EventsLoop,
+) -> impl Stream<Item = Vec<Event>, Error = timer::Error> {
+    let frame_interval = Interval::new_interval(Duration::from_millis(16));
+    frame_interval.map(move |_now| {
         let mut events = Vec::new();
         events_loop.poll_events(|event| events.push(event));
-
-        self.last_update = Instant::now();
-
         events
-    }
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 // Very much based on https://github.com/PistonDevelopers/conrod/blob/master/examples/hello_world.rs
 
-mod source;
 mod event_loop;
+mod source;
 
-use conrod::backend::glium::glium::glutin::{ContextBuilder, Event, EventsLoop, WindowBuilder, WindowEvent};
 use conrod::backend::glium::glium::glutin::dpi::LogicalSize;
+use conrod::backend::glium::glium::glutin::{
+    ContextBuilder, Event, EventsLoop, WindowBuilder, WindowEvent,
+};
 use conrod::backend::glium::glium::texture::Texture2d;
 use conrod::backend::glium::glium::{Display, Surface};
 use conrod::backend::glium::Renderer;
 use conrod::text::Font;
-use conrod::{image, widget, widget_ids, UiBuilder, UiCell, Positionable, Widget, Colorable, Labelable, Sizeable};
+use conrod::{
+    image, widget, widget_ids, Colorable, Labelable, Positionable, Sizeable, UiBuilder, UiCell,
+    Widget,
+};
 
-use event_loop::EventLoop;
+use event_loop::event_stream;
+
+use futures::prelude::*;
 
 use std::process;
 
@@ -23,11 +30,7 @@ widget_ids! {
 }
 
 // FIXME: Fetch dynamically instead
-static CYCLISTS: [&'static str; 3] = [
-    "Jens",
-    "Erik",
-    "Johan",
-];
+static CYCLISTS: [&'static str; 3] = ["Jens", "Erik", "Johan"];
 
 enum Page {
     Login,
@@ -41,47 +44,50 @@ struct State {
 
 impl State {
     fn new() -> State {
-        State {
-            page: Page::Login,
-        }
+        State { page: Page::Login }
     }
 }
 
-fn render(state: &State, ids: &Ids, ui: &mut UiCell) {
+fn render(state: &mut State, ids: &Ids, ui: &mut UiCell) {
     match state.page {
         Page::Login => {
-            widget::Text::new("Who are you?").middle_of(ui.window).color(conrod::color::WHITE).font_size(32).set(ids.login_page_header, ui);
-            let (mut items, scrollbar) = widget::List::flow_down(CYCLISTS.len()).h(150.0).scrollbar_next_to().scrollbar_color(conrod::color::WHITE).item_size(30.0).set(ids.login_page_user_list, ui);
+            widget::Text::new("Who are you?")
+                .middle_of(ui.window)
+                .color(conrod::color::WHITE)
+                .font_size(32)
+                .set(ids.login_page_header, ui);
+            let (mut items, scrollbar) = widget::List::flow_down(CYCLISTS.len())
+                .h(150.0)
+                .scrollbar_next_to()
+                .scrollbar_color(conrod::color::WHITE)
+                .item_size(30.0)
+                .set(ids.login_page_user_list, ui);
             while let Some(item) = items.next(ui) {
-                if item.set(widget::Button::new().label(CYCLISTS[item.i]), ui).was_clicked() {
+                if item
+                    .set(widget::Button::new().label(CYCLISTS[item.i]), ui)
+                    .was_clicked()
+                {
                     println!("{}", CYCLISTS[item.i]);
                 }
             }
             if let Some(scrollbar) = scrollbar {
                 scrollbar.set(ui);
             }
-        },
-        Page::Connecting => {
-            unreachable!()
         }
-        Page::Cycling => {
-            unreachable!()
-        },
+        Page::Connecting => unreachable!(),
+        Page::Cycling => unreachable!(),
     }
 }
 
 fn main() {
     let (width, height) = (320.0, 240.0);
 
-    let mut events_loop = EventsLoop::new();
-    let mut event_loop = EventLoop::new();
+    let events_loop = EventsLoop::new();
 
     let window = WindowBuilder::new()
         .with_title("BikeTracker")
         .with_dimensions(LogicalSize::new(width, height));
-    let context = ContextBuilder::new()
-        .with_vsync(true)
-        .with_multisampling(2);
+    let context = ContextBuilder::new().with_vsync(true).with_multisampling(2);
     let display = Display::new(window, context, &events_loop).unwrap();
     let image_map = image::Map::<Texture2d>::new();
     let mut renderer = Renderer::new(&display).unwrap();
@@ -89,22 +95,28 @@ fn main() {
     let mut ui = UiBuilder::new([width as f64, height as f64]).build();
     let ids = Ids::new(ui.widget_id_generator());
 
-    let noto_sans = Font::from_bytes(&include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/NotoSans-Regular.ttf"))[..]).unwrap();
+    let noto_sans = Font::from_bytes(
+        &include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/NotoSans-Regular.ttf"))[..],
+    )
+    .unwrap();
     ui.fonts.insert(noto_sans);
 
-    {
-        let mut state = State::new();
-
-        'main: loop {
-            for event in event_loop.next(&mut events_loop) {
-                match event.clone() {
-                    Event::WindowEvent { event, .. } => match event {
-                        WindowEvent::CloseRequested => break 'main,
-                        _ => {}
-                    },
+    let mut state = State::new();
+    let main_loop = event_stream(events_loop)
+        .take_while(|events| {
+            for event in events.iter() {
+                match &event {
+                    Event::WindowEvent {
+                        event: WindowEvent::CloseRequested,
+                        ..
+                    } => return Ok(false),
                     _ => {}
                 }
-
+            }
+            Ok(true)
+        })
+        .for_each(move |events| {
+            for event in events {
                 // Use the `winit` backend feature to convert the winit event to a conrod input.
                 let input = match conrod::backend::winit::convert_event(event, &display) {
                     None => continue,
@@ -115,7 +127,7 @@ fn main() {
 
             let ui = &mut ui.set_widgets();
             // update_channels(&mut state);
-            render(&state, &ids, ui);
+            render(&mut state, &ids, ui);
             // TODO: maybe hook the file I/O into the event loop somehow?
 
             if let Some(primitives) = ui.draw_if_changed() {
@@ -126,8 +138,10 @@ fn main() {
                 renderer.draw(&display, &mut target, &image_map).unwrap();
                 target.finish().unwrap();
             }
-        }
-    }
+            Ok(())
+        });
+
+    tokio::runtime::current_thread::block_on_all(main_loop).unwrap();
 
     process::exit(0);
 }
